0b595812629b30601dccd1444d2c455d4691e68e
ChrisRDavila <crossdavila@gmail.com>
2023-11-17 13:36:35 -0800

    updating UI to use input variable in response, some issues, retooling Class and index.js

--- src/index.js
+++ src/index.js
@@ -18,25 +18,35 @@ function getRates(amount, type) {
 
 // UI Logic
 
-// function printElements(response, amount, type) {
-//   document.querySelector('#showResponse').innerText = `The humidity in ${city} is ${response.main.humidity}%.
-//   The temperature in Kelvins is ${response.main.temp} degrees.`;
-// }
+function printElements(response) {
+  const amount = document.querySelector('#amount').value;
+  const type = document.querySelector('#type').value.toUpperCase();
+  const rates = response[0];
+  const resultRate = rates.conversion_rates[type];
+  const resultAmount  = (resultRate * amount).toFixed(2);
+  if (rates.conversion_rates[type] === undefined) {
+    document.querySelector('#show-results').innerText = `${response[2]} is not a real country code`;
+    document.querySelector('#type').value = null;
+    document.querySelector('#amount').value = null;
+  } else { 
+    document.querySelector("#show-results").innerHTML = `<p>Your exchange from ${response[1]}$ USD<p>
+    <p>in ${response[2]} equals ${resultAmount}<p>`;
+    document.querySelector('#type').value = null;
+    document.querySelector('#amount').value = null;
+  }  
+}
 
-// function printError(error, amount, type) {
-//   document.querySelector('#showResponse').innerText = `There was an error accessing the weather data for ${city}: 
-//   ${error}.`;
-// }
+function printError(error) {
+  document.querySelector('#show-results').innerText = `There was an error accessing the data from exchange rate API ${error}`;
+}
 
-// function handleFormSubmission(event) {
-//   event.preventDefault();
-//   const amount = document.querySelector('#amount').value;
-//   const type = document.querySelector('#type').value;
-//   document.querySelector('#amount').value = null;
-//   document.querySelector('#type').value = null;
-//   getRates(amount, type);
-// }
+function handleFormSubmission(event) {
+  event.preventDefault();
+  const type = document.querySelector('#type').value; 
+  const amount = document.querySelector('#amount').value;
+  getRates(amount, type);
+}
 
-// window.addEventListener("load", function() {
-//   document.querySelector('form').addEventListener("submit", handleFormSubmission);
-// });
+window.addEventListener("load", function() {
+  document.querySelector('form').addEventListener("submit", handleFormSubmission);
+});
--- src/sevices/Exchange-service.js
+++ src/sevices/Exchange-service.js
@@ -1,12 +1,12 @@
 export default class Exchange {  
-  static getExchangeAPI(amount, type) {
+  static getExchangeAPI() {
     return fetch(`https://v6.exchangerate-api.com/v6/${process.env.API_KEY}/latest/USD`)
       .then(function(response) {
         if (!response.ok) {
           const errorMessage = `${response.status} ${response.statusText}`;
           throw new Error(errorMessage);
         } else {
-          return [response.json(), amount, type];
+          return response.json();
         }
       })      
       .catch(function(error) {
